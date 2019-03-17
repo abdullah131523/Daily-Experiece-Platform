eec7072926664646fc0cec8364f243271dc0cf26
AbdulazezShikh <43558592+AbdulazezShikh@users.noreply.github.com>
2019-03-17 14:49:58 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,39 @@
+    <ul>
+      <li class = "headerStyle"> <a href = "Login.php"> <i class="glyphicon glyphicon-log-in"> </i>تسجيل الدخول  </a> </li>
+      <li class = "headerStyle"> <a href = "SignUp.php"> <i class="glyphicon glyphicon-user"></i> تسجيل </a> </li>
+   </ul>
+
+      <div class="search">
+        <form>
+          <input type="text" placeholder="البحث" name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+      </div>
+    
+  <div id="menu">
+  <ul id = "position">
+
+  <li> <a href="HomePage.php"> الصفحة الرئيسية</a></li>
+  <li> <a href="NewPost.php"> مشاركة </a></li>
+  <li> <a href="PersonalPage.php">  الصفحة الشخصية</a></li>
+  <li> <a href="CommunicationPage.php"> الدعم الفني </a></li>
+
+<li>  <a href="#"> الأقسام </a>
+
+<ul>
+
+   <li> <a href="#"> التصميم </a></li> 
+  <li> <a href="#"> الفن والديكور </a></li> 
+   <li>  <a href="#"> الأعمال المنزلية </a></li> 
+   <li> <a href="#"> الطبخ </a></li>
+   <li> <a href="#"> الألعاب </a></li> 
+    <li> <a href="#"> السيارات </a></li>
+
+</ul>
+
+</li>
+  </ul>
+    
+</div>
+   
+
