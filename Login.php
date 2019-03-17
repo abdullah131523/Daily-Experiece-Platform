206f288666b0d5e6dc2e549c24ec7e7cac9e25d5
abdullah <abdullah131523@gmail.com>
2019-03-17 13:32:27 +0300

    after add session and encrypt password

--- Login.php
+++ Login.php
@@ -1,4 +1,7 @@
+<?php
+session_start();
 
+?>
 <!DOCTYPE html>
 
 <html>
@@ -6,7 +9,6 @@
    <head>
 
        <meta charset="utf-8" />
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <title> صفحة تسجيل الدخول </title> 
       <link rel="stylesheet" type="text/css" href="HomePageStyle.css">
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -16,14 +18,15 @@
 <body>
     
      <?php 
-      session_start();
-      $db = mysqli_connect('localhost','abdulazez','','Daily Experience Platform');
-   
+
+      require 'db_connection.php';
+
+     
+      $emailErr = "";
+      $passwordErr = "";
   if (isset($_POST["submit"])){ 
   
-    $_SESSION["em"] =  $_POST["em"];
-                $_SESSION["pass"] = $_POST["pass"];
-                 $_SESSION["Last_active"] = time();
+               
                 
              
             if (!empty($_POST["em"]) && !empty($_POST["pass"])){
@@ -32,15 +35,31 @@
                $password = $_POST['pass'];
            
     
+               $password_encrypt = hash("sha512",$password);
+          
+                
+                 $sql_email = "select * from Creators where email = '$email'";
+                 $sql_password = "select * from Creators where password = '$password_encrypt' ";
+                 $statement_email  = mysqli_query($db,$sql_email);
+                 $statement_password  = mysqli_query($db,$sql_password);
    
-   
+                
+             if (mysqli_num_rows ($statement_email)  < 1){
+    $emailErr = "  لا يوجد حساب مسجل من قبل بهذا الايميل ... الرجاء إدخال حساب صحيح";
+     }else if (mysqli_num_rows($statement_password) < 1){
+     $passwordErr = " < كلمة المرور غير صحيحة ... إذا نسيت كلمة المرور تفضل بالنقر على رابط < نسيت كلمة المرور؟";
+     }else{
+ 
 
-    $sql = "select email and password from Creators where email = '$email'  and   password = '$password'";
+    $sql = "select email and password from Creators where email = '$email'  and   password = '$password_encrypt'";
   
     $statement  = mysqli_query($db,$sql);
    
     if (mysqli_num_rows($statement) == 1){
- 
+    
+        $_SESSION["em"] =  $email;
+                 $_SESSION["pass"] = $password;
+                 $_SESSION["Last_active"] = time();
   
     
     header("Location:HomePage.php");
@@ -50,41 +69,13 @@
              
     }
     }
+  }
+    }
+          include 'header.php';
+
     ?>
     
     
-    
-    <ul>
-      <li class = "headerStyle"> <a href = "#"> <i class="glyphicon glyphicon-log-in"> </i>تسجيل الدخول  </a> </li>
-      <li class = "headerStyle"> <a href = "#"> <i class="glyphicon glyphicon-user"></i> تسجيل </a> </li>
-   </ul>
-
-    
-<div id="menu">
-  <ul id = "position">
-
-  <li> <a href="#"> الصفحة الرئيسية</a></li>
-  <li> <a href="#"> مشاركة </a></li>
-   <li> <a href="#">  الصفحة الشخصية</a></li>
-  <li> <a href="#"> الدعم الفني </a></li>
-
-<li>  <a href="#"> الأقسام </a>
-
-<ul>
-
-   <li> <a href="#"> التصميم </a></li> 
-  <li> <a href="#"> الفن والديكور </a></li> 
-   <li>  <a href="#"> الأعمال المنزلية </a></li> 
-   <li> <a href="#"> الطبخ </a></li>
-   <li> <a href="#"> الألعاب </a></li> 
-    <li> <a href="#"> السيارات </a></li>
-
-</ul>
-
-</li>
-  </ul>
-    
-</div>
    
 
 
@@ -94,8 +85,9 @@
          
          
   <div>
-       <input type="email" name="em"  id = "email" placeholder="الإيميل"   > <!--to display in field--> 
+       <input type="email" name="em"  id = "email" placeholder="الإيميل"   >  
        <div id = email_error class = "content_error" ></div>
+        <span class="error"> <?php echo $emailErr;?></span>
      
   </div>
   
@@ -103,14 +95,17 @@
  <div>
        <input type="password" id="password" name="pass" placeholder="كلمة المرور"   >;  
        <div id = "password_error"    class = "content_error"></div>
-      
+        <span class="error"> <?php echo $passwordErr;?></span>
  </div>
  
      
           <a id = "forget"  href="ForgetPassword.php" > نسيت كلمة المرور؟</a>
 
  <input type="submit" name="submit"  value = "تسجيل الدخول">
- --<script  src="JS.js"></script>
+         
+    </form>
+ <script  src="JS.js"></script>
+         
 </body>
 
 </html>
